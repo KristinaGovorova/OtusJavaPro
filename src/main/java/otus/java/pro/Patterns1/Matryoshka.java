b7589ce9583b90ff9781141a60f79e9aaa28f586
KristinaGovorova <k.govorova94@mail.ru>
2024-11-19 19:31:41 +0300

    HW5 - Iterator
    HW5 - Iterator

---
+++ src/main/java/otus/java/pro/Patterns1/Matryoshka.java
@@ -0,0 +1,20 @@
+package otus.java.pro.Patterns1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public final class Matryoshka {
+    // [0] - the smallest / [9] - the biggest
+    private final List<String> items;
+
+    public Matryoshka(String color) {
+        items = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            items.add(color + i);
+        }
+    }
+
+    public List<String> getItems() {
+        return items;
+    }
+}
